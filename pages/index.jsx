854246a895d5614b606bc5b267c9384735f30da1
RenatoGSantos <39703265+renatogsantos@users.noreply.github.com>
2023-03-14 15:05:46 -0300

    teste de dados novos

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,34 +4,35 @@ import axios from "axios";
 import Head from "next/head";
 import { useEffect, useMemo, useState } from "react";
 import { Col, Container, Row } from "react-bootstrap";
+import { gamesList } from "../config/games";
 
 export default function Home() {
   const [searchValue, setSearchValue] = useState("");
-  const [gamesList, setGamesList] = useState([]);
+  //const [gamesList, setGamesList] = useState([]);
 
   const listSearch = gamesList.filter((item) =>
     item.title.toLowerCase().includes(searchValue.toLowerCase())
   );
 
-  useEffect(() => {
-    const options = {
-      method: "GET",
-      url: "https://free-to-play-games-database.p.rapidapi.com/api/games",
-      headers: {
-        "X-RapidAPI-Key": process.env.X_RAPIDAPI_KEY,
-        "X-RapidAPI-Host": process.env.X_RAPIDAPI_HOST,
-      },
-    };
+  // useEffect(() => {
+  //   const options = {
+  //     method: "GET",
+  //     url: "https://free-to-play-games-database.p.rapidapi.com/api/games",
+  //     headers: {
+  //       "X-RapidAPI-Key": process.env.X_RAPIDAPI_KEY,
+  //       "X-RapidAPI-Host": process.env.X_RAPIDAPI_HOST,
+  //     },
+  //   };
 
-    axios
-      .request(options)
-      .then(function (response) {
-        setGamesList(response.data);
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
-  }, []);
+  //   axios
+  //     .request(options)
+  //     .then(function (response) {
+  //       setGamesList(response.data);
+  //     })
+  //     .catch(function (error) {
+  //       console.error(error);
+  //     });
+  // }, []);
 
   return (
     <>
@@ -48,25 +49,33 @@ export default function Home() {
             <Row className="d-flex align-items-center g-2">
               <Col sm="12" lg="6">
                 <div className="position-relative">
+                  <img
+                    draggable={false}
+                    className="vector-play"
+                    src="/vector-play.svg"
+                    alt=""
+                  />
                   <h1>GAMESPHERE</h1>
-                  <h2 className="text-end">
-                    <GameController size={32} weight="duotone" /> Baixe Jogos
+                  <h2>
+                    <GameController size={38} weight="duotone" /> Baixe Jogos
                     Gratuitos
                   </h2>
-                  <small className="text-end">O jogo que você procura está aqui!</small>
-                  <div className="input-main-box pt-5">
-                    <input
-                      className="input-main"
-                      type="text"
-                      placeholder="Faça sua busca..."
-                      onChange={(e) => {
-                        setSearchValue(e.target.value);
-                      }}
-                    />
-                  </div>
+                  <p className="pt-4 f-12">
+                    Bem-vindo ao GameSphere, o lugar perfeito para encontrar e
+                    baixar jogos gratuitamente. Estamos animados em oferecer aos
+                    nossos usuários uma ampla seleção de jogos em vários
+                    gêneros, desde jogos de ação até jogos de simulação. Com uma
+                    interface amigável e fácil de usar, estamos comprometidos em
+                    fornecer a melhor experiência de usuário possível. Nossa
+                    equipe está trabalhando duro para garantir que todos os
+                    jogos em nosso site sejam seguros para download e estejam
+                    disponíveis para você gratuitamente. Obrigado por visitar o
+                    Gameflix e esperamos que você aproveite nossa seleção de
+                    jogos gratuitos.
+                  </p>
                 </div>
               </Col>
-              <Col sm="12" lg="6">
+              <Col sm="12" lg="6" className="d-none d-lg-flex">
                 <div className="d-flex align-items-center justify-content-center w-100">
                   <img
                     draggable={false}
@@ -83,9 +92,21 @@ export default function Home() {
 
       <section>
         <Container fluid className="py-5">
-          <Container fluid>
-            <Row className="d-flex align-items-stretch justify-content-center g-4">
-              {listSearch.slice(0, 12).map((item) => {
+          <Container>
+            <div className="input-main-box py-5">
+              <input
+                className="input-main"
+                type="text"
+                placeholder="Faça sua busca..."
+                onChange={(e) => {
+                  setSearchValue(e.target.value);
+                }}
+              />
+            </div>
+          </Container>
+          <Row className="d-flex align-items-stretch justify-content-center g-4">
+            {listSearch ? (
+              listSearch.slice(0, 12).map((item) => {
                 return (
                   <Col key={item.id} sm="12" md="6" lg="4" xl="3" xxl="2">
                     <CardGame
@@ -97,11 +118,14 @@ export default function Home() {
                     />
                   </Col>
                 );
-              })}
-            </Row>
-          </Container>
+              })
+            ) : (
+              <div className="d-flex align-items-center justify-content-center py-5">
+                <p>Lista vazia...</p>
+              </div>
+            )}
+          </Row>
         </Container>
-        <ul></ul>
       </section>
     </>
   );
